87bea8dc49a43af2d8148b81a6a22d4e93e6dfb9
Ralakash <shawnkaralash@gmail.com>
2023-07-14 15:38:59 -0400

    adds previously added classes and styling to page for bio section.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import NavBar from './Components/navBar/NavBar';
+import Bio from './Components/body/Bio';
+import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 
 function App() {
-  return (
-    <div className='container'>
-      <NavBar />
-    </div>
-  );
+	return (
+		<div className="w-100 h-100" id="border-start">
+      <div
+        id="border-end"
+        className="w-100 h-100 row"
+      >
+			<NavBar />
+			<main className="row">
+				<Bio />
+			</main>
+      </div>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/Components/body/Bio.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+function Bio() {
+	return (
+        <section  className="About-Me col-6 row m-3"
+        id="About-Me">
+        <h2 className="col-4">Who I am...</h2>
+		<p className="col-8">
+			My name is Shawn Karalash. I was born in Kalamazoo, Michigan, then lived in various states
+			throughout my life. I have found my way back to Kalamazoo and currently live here with my wife
+			and our three dogs and three cats. <br />
+			<br />
+			In 2007, I completed my Bachelors degree in Psychology at Western Michigan University. Leading
+			me to work in Children’s Protective Services for the State of Michigan Department of Health
+			and Human Services Child Welfare department. Prior to my current position, I was working in
+			the Foster Care field for three years. My work experience prior to working with MDHHS
+			includes, working with the United States Postal Service for two and a half years as a letter
+			carrier; six and half years with PNC bank providing financial services to clients. <br />
+			<br />
+			In my free time, I enjoy hiking and being outdoors. Traveling is one of my favorite pastimes
+			and almost all of my travels are road trips. Saturdays I enjoy participating in a Dungeons and
+			Dragons game being the Dungeon Master, which I have been doing on and off for the last five
+			years. I also enjoy playing board games, as well as video games.
+		</p>
+        </section>
+	);
+}
+
+export default Bio;
