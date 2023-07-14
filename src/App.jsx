a3b3ea340572ee983eeb4d2f337d885ebb4ed8bf
shawn karalash <shawnkaralash@gmail.com>
2023-07-14 08:34:08 -0400

    change over to react

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import NavBar from './Components/navBar/NavBar';
+import './App.css';
+
+function App() {
+  return (
+    <div className='container'>
+      <NavBar />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/navBar/NavBar.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import { useLayoutEffect, useState } from 'react';
+import Pdf from '../../assets/Resume2023.pdf';
+import HeadShot from '../../assets/images/profile-picture.jpg';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope, faFileText, faBars } from '@fortawesome/free-solid-svg-icons';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+
+function useWindowSize() {
+  const [size, setSize] = useState([0, 0]);
+  useLayoutEffect(() => {
+    function updateSize() {
+      setSize([window.innerWidth, window.innerHeight]);
+    }
+    window.addEventListener('resize', updateSize);
+    updateSize();
+    return () => window.removeEventListener('resize', updateSize);
+  }, []);
+  return size;
+}
+
+function NavBar() {
+  const [width, height] = useWindowSize();
+  const inlineDisplay = (
+    <ul className='container justify-content-end'>
+      <li className='m-2 display-6'>
+        <a href='https://github.com/Ralakash'>
+          <FontAwesomeIcon icon={faGithub} title='Github' />
+        </a>
+      </li>
+      <li className='m-2 display-6'>
+        <a href='mailto: shawnkaralash@gmail.com'>
+          <FontAwesomeIcon icon={faEnvelope} title='Email' />
+        </a>
+      </li>
+      <li className='m-2 display-6'>
+        <a href={Pdf} target='_blank'>
+          <FontAwesomeIcon icon={faFileText} title='Resume' />
+        </a>
+      </li>
+    </ul>
+  );
+
+  const hamburgerMenu = <FontAwesomeIcon icon={faBars} title='Menu' />;
+
+  return (
+    <header className='col-12'>
+      <h1 className='col'>
+        <img src={HeadShot} alt='Headshot' />
+      </h1>
+      <nav className='col'>{width <= 500 ? hamburgerMenu : inlineDisplay}</nav>
+    </header>
+  );
+}
+
+export default NavBar;
